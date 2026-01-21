8c005f57989294031712d53d852ae9c260ff8dce
isaacmatovu <matovuisaac646@gmail.com>
2026-01-21 13:25:29 +0300

    added about page

---
+++ app/about/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/about/header";
+import Industries from "../components/about/industries";
+import Overview from "../components/about/main";
+
+function About() {
+  return (
+    <div>
+      <Header />
+      <Industries />
+      <Overview />
+    </div>
+  );
+}
+
+export default About;
