d25168b11077fd2e1acf5418f29aa79821bb702f
isaacmatovu <matovuisaac646@gmail.com>
2026-01-24 23:30:11 +0300

    added service page

---
+++ app/services/page.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../components/services/header";
+import Services from "../components/services/serviceCards";
+import LetsWork from "../components/services/letsWork";
+
+function ServicesPage() {
+  return (
+    <div>
+      <Header />
+      <Services />
+      <LetsWork />
+    </div>
+  );
+}
+
+export default ServicesPage;
