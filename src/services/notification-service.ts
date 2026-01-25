eba8f31b2d7d8ddfb8360fd9de3321b7565b5af6
kasimlyee <kasiimlyee@gmail.com>
2026-01-25 11:05:53 +0300

    added services

--- src/services/notification-service.ts
+++ src/services/notification-service.ts
@@ -1,12 +1,27 @@
 import nodemailer from 'nodemailer';
 import { Contact, ContactType } from '../entities/Contact.entity';
+import {env} from '../../env.schema'
 
 export class NotificationService{
     private transporter: nodemailer.Transporter;
 
     constructor(){
         this.transporter = nodemailer.createTransport({
-            host: 
-        })
+            host: env.SMTP_HOST,
+            port: env.SMTP_PORT,
+            secure: env.SMTP_SECURE === 'true',
+            auth: {
+                user: env.SMTP_USER,
+                pass: env.SMTP_PASS
+            },
+        });
+    }
+
+    async sendNotification(contact: Contact):Promise<void>{
+        await this.sendInternalNotification(contact);
+
+        await this.sendUserConfirmation(contact);
+
+        if(contact.type === ContactType.WHATSAPP && contact.phone cont)
     }
 }
