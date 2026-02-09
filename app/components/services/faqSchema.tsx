caf16e82c0136c3338f8fa29d158971d34e3f079
isaacmatovu <matovuisaac646@gmail.com>
2026-02-09 08:44:49 +0300

    added faqsection schema

---
+++ app/components/services/faqSchema.tsx
@@ -0,0 +1,25 @@
+import Script from "next/script";
+import { faqs } from "./faqData";
+
+export function FAQSchema() {
+  return (
+    <Script
+      id="services-faq-schema"
+      type="application/ld+json"
+      dangerouslySetInnerHTML={{
+        __html: JSON.stringify({
+          "@context": "https://schema.org",
+          "@type": "FAQPage",
+          mainEntity: faqs.map((faq) => ({
+            "@type": "Question",
+            name: faq.question,
+            acceptedAnswer: {
+              "@type": "Answer",
+              text: faq.answer,
+            },
+          })),
+        }),
+      }}
+    />
+  );
+}
